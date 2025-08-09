60ebc453d27daa3c4423c66d65890cfb164c9eda
zeppombal <79043513+zeppombal@users.noreply.github.com>
2025-08-09 15:47:07 +0100

    Update base_inference.py

--- model_inference/base_inference.py
+++ model_inference/base_inference.py
@@ -1,9 +1,19 @@
-import json, os
+import json
+import os
+
 
 class BaseHandler:
     model_name: str
 
-    def __init__(self, model_name, model_path=None, temperature=0.7, top_p=1, max_tokens=1000, language = "zh") -> None:
+    def __init__(
+        self,
+        model_name,
+        model_path=None,
+        temperature=0.7,
+        top_p=1,
+        max_tokens=1000,
+        language="zh",
+    ) -> None:
         self.model_name = model_name
         self.temperature = temperature
         self.top_p = top_p
@@ -14,12 +24,10 @@ class BaseHandler:
         # This method is used to retrive model response for each model.
         pass
 
-
     def write_result(self, result, model_name, result_path):
 
-        
         os.makedirs(f"{result_path}{model_name}", exist_ok=True)
-        
+
         if type(result) is dict:
             result = [result]
 
@@ -28,7 +36,9 @@ class BaseHandler:
                 test_category = entry["id"].rsplit("_", 1)[0]
             else:
                 test_category = "_".join(entry["id"].split("_")[:-2])
-            file_to_write = f"{result_path}{model_name}/data_{test_category}_result.json"
-        
+            file_to_write = (
+                f"{result_path}{model_name}/data_{test_category}_result.json"
+            )
+
             with open(file_to_write, "a+", encoding="utf-8") as f:
                 f.write(json.dumps(entry, ensure_ascii=False) + "\n")
